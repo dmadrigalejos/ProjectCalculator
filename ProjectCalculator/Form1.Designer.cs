c91c89f0c42421ba71e3912ced984cfe5d74a87c
Danilo Madrigalejos <dan@yahoo.com>
2021-05-15 14:31:41 +0800

    Added button2

--- ProjectCalculator/Form1.Designer.cs
+++ ProjectCalculator/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnOne = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOne
@@ -40,11 +41,21 @@
             this.btnOne.Text = "1";
             this.btnOne.UseVisualStyleBackColor = true;
             // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(113, 155);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn2);
             this.Controls.Add(this.btnOne);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btn2;
     }
 }
 
